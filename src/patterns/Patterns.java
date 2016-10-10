62f70da1d42008b002867bf3bcaedfe423bc6a7f
Mirek Kowalski <mir_kow@wp.pl>
2016-10-10 12:01:32 +0200

    new file:   .classpath 	new file:   .project 	new file:   .settings/org.eclipse.jdt.core.prefs 	modified:   src/patterns/Patterns.java

--- src/patterns/Patterns.java
+++ src/patterns/Patterns.java
@@ -5,6 +5,7 @@ public class Patterns {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Wzorce");
+		System.out.println("...i procesy");
 	}
 
 }
